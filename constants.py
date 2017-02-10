5876a6ac50b157b1557f4d2e3f28c82f6db362a8
Paul Duffy <paul.duffy@netan.solutions>
2017-02-10 07:14:04 +0000

    Added files for the first time

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'RoKyiZAefrQkUwJvlUIQtadCw'
+CONSUMER_SECRET = 'dSoy5j8v4hVJ0SEDPYOwKVC1QPhDNaZljgbKO2knj03JxI2B9s'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
